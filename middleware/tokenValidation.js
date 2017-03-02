3b2551a0ecdc857d3b19c63565a2c8a183ee1368
kumar arunav rout <arunav.rout93@gmail.com>
2017-03-02 12:17:00 +0530

    added logger in validation

--- middleware/tokenValidation.js
+++ middleware/tokenValidation.js
@@ -4,6 +4,7 @@ var evaluationcontroller = require('../controllers/evaluationController');
 var testcontroller = require('../controllers/testcontroller');
 var evalsummarycontroller = require('../controllers/evalsummarycontroller');
 var getSecret = require('../services/key');
+var logger = require('../logger')
 
 module.exports = function(app,canauthrouter){
 
@@ -11,22 +12,24 @@ module.exports = function(app,canauthrouter){
     var token = request.headers['acc-token'];
 
     getSecret.getKey(function(key){
+        logger.info(token+':token received');
         if(token){
             jwt.verify(token,key,function(err,decode){
               if(err){
+                logger.error('err:'+ err);
                 response.status(500).json("Invalid Token");
               }
               else{
                 console.log(request.url);
                 var URL = String(request.url).split('=');
-                console.log(URL[0]);
+                logger.info(URL[0]);
                 if((request.url=='/submit' || request.url=='/questionset') && decode.Auth_Level == 10){
-                  console.log(decode)
+                  logger.info(decode);
                   rem1(decode.Email,decode.Password);
                   next();
                 }
                 else if(((URL[0])=='/answers?Email' || request.url=='/submiteval' || request.url=='/saveeval' || request.url=='/summary') && (decode.Auth_Level == 30 || decode.Auth_Level == 20)){
-                  console.log(decode)
+                  logger.info(decode);
                   rem2();
                   next();
                 }
@@ -43,6 +46,7 @@ module.exports = function(app,canauthrouter){
   });
 
   var rem1 = function(id,pw){
+    logger.info(id+':id'+','+pw+':pw');
     testcontroller(app, canauthrouter,id,pw);
   }
   var rem2 = function()
