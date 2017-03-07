ad3e89e424af3013c5817903e0bb4af7e338d122
kumar arunav rout <arunav.rout93@gmail.com>
2017-03-07 10:53:36 +0530

    Add email and Reg_DT to summary

--- services/evalsummary.js
+++ services/evalsummary.js
@@ -3,12 +3,30 @@ var config = require('../config.js');
 
 exports.getCandidateInfo = function(query,callback){
   var request = new sql.Request();
-  request.query('select ID,Name,Experience,Skill,Score,Consultant_Name,Logic_Score,Status from Candidate', function(err,recordset){
+  request.query('select ID,Name,Email,Experience,Registration_Date,Skill,Score,Consultant_Name,Logic_Score,Status from Candidate', function(err,recordset){
     if(err != null){
       console.log(err);
     }
     else{
-      callback(recordset);
+      console.log('inside else');
+      console.log(recordset.length);
+      if(recordset.length > 0){
+        for (var i = 0; i < recordset.length; i++){
+          console.log('inside for');
+          console.log(typeof recordset[i].Registration_Date);
+          console.log(recordset[i].Registration_Date);
+          recordset[i].Registration_Date = JSON.stringify(recordset[i].Registration_Date).replace('"',"").split("T")[0];
+          console.log(recordset[i].Registration_Date);
+          console.log(i);
+          if(i === recordset.length -1){
+            console.log(recordset);
+            callback(recordset);
+          }
+        }
+     }
+     else{
+       callback(recordset);
+     }
     }
   });
   }
